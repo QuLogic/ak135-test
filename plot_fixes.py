d981f5d9ee5f75aaf7ce895c76711d52fe9fb721
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2014-11-12 17:48:57 -0500

    Add new fix tests to plotter.

--- plot_fixes.py
+++ plot_fixes.py
@@ -13,26 +13,25 @@ def get_file(run, seismo):
     else:
         return os.path.join(run, seismo)
 
-# Commits tested on all compilers
-ALL_COMMITS = [
-    ('6994fe6', 'devel'),
-    ('a87d120', 'static-analysis')
-]
-
 # Commits test on ifort only
 IFORT_COMMITS = [
     ('a87d120', 'static-analysis'),
     ('d778d2c', 'fp-model=strict'),
     ('2aaca03', 'fp-model=source'),
     ('cdd5c9d', 'standard-semantics'),
-    ('eaa440b', 'standard-semantics without realloc-lhs')
+    ('eaa440b', 'standard-semantics without realloc-lhs'),
+    ('205eda4', 'standard-semantics w/o realloc + parentheses'),
+    ('6306dfe', 'standard-semantics w/o realloc + rearrange'),
+    ('4bb8531', 'standard-semantics w/o realloc + refactor'),
+    ('38c778d', 'standard-semantics w/o realloc + refactor 2'),
+    ('ce205ee', 'refactor only'),
 ]
 
 
 NEX = [96, 144, 192, 240]
 DISTS = [101, 101, 45, 90]
 COMPILERS = ['ifort', 'gfortran', 'xlf', 'xlf_strict']
-MARKER_SPACING = [13, 17, 23, 29, 31, 37]
+MARKER_SPACING = [13, 17, 23, 29, 31, 37, 41, 43, 47, 53]
 
 
 # Compare ifort only for its fixes
@@ -53,7 +52,7 @@ for i, (nex, dist) in enumerate(zip(NEX, DISTS)):
     elif 60 <= dist < 90:
         axes[2].set_xlim(2000, 3000)
     elif 90 <= dist:
-        axes[2].set_xlim(3000, data[-1,0])
+        axes[2].set_xlim(3250, 3400)
     axes[2].set_xlabel('Time')
     axes[0].legend(loc='best')
     fig.suptitle('ifort check, %d deg, NEX=%d' % (dist, nex))
